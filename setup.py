01db8cdeeabf2212dbae03384d80641961c9d8d3
Andrew Roth <andrewjlroth@gmail.com>
2017-11-04 13:16:54 +0000

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+description = '''Wrapper library for bioinformatic tools.'''
+
+setup(
+    name='pot',
+    version='0.1.0',
+    description=description,
+    author='Andrew Roth',
+    author_email='andrewjlroth@gmail.com',
+    url='https://github.com/aroth85/pot',
+    packages=find_packages(),
+)
