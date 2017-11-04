ec04bbcc09f46b2901c3947042b87e5c19eedaaa
Andrew Roth <andrewjlroth@gmail.com>
2017-11-04 13:08:00 +0000

    feature: workflow for single lane wgs alingment

---
+++ pot/workflows/wgs_align.py
@@ -0,0 +1,62 @@
+from pypeliner.sandbox import CondaSandbox
+
+import pypeliner
+import pypeliner.managed as mgd
+
+import pot.wrappers.bwa
+import pot.wrappers.sambamba
+
+
+def create_single_lane_alignment_workflow(
+        fastq_file_1,
+        fastq_file_2,
+        ref_genome_fasta_file,
+        out_bam_file,
+        bwa_threads=1,
+        read_group_info=None,
+        sambamba_threads=1):
+
+    sandbox = CondaSandbox(
+        channels=('bioconda',),
+        packages=('bwa ==0.7.16', 'samtools ==1.6', 'sambamba ==0.6.6'),
+    )
+
+    workflow = pypeliner.workflow.Workflow(sandbox=sandbox)
+
+    workflow.transform(
+        name='bwa_mem_paired_end',
+        ctx={'mem': 32, 'mem_retry_increment': 16, 'num_retry': 3, 'num_cpus': bwa_threads},
+        func=pot.wrappers.bwa.mem_paired_end,
+        args=(
+            mgd.InputFile(fastq_file_1),
+            mgd.InputFile(fastq_file_2),
+            mgd.InputFile(ref_genome_fasta_file),
+            mgd.TempOutputFile('aligned.bam'),
+        ),
+        kwargs={
+            'read_group_info': read_group_info,
+            'threads': bwa_threads,
+        }
+    )
+
+    workflow.transform(
+        name='sort',
+        ctx={'mem': 32, 'mem_retry_increment': 16, 'num_retry': 3, 'num_cpus': sambamba_threads},
+        func=pot.wrappers.sambamba.sort,
+        args=(
+            mgd.TempInputFile('aligned.bam'),
+            mgd.OutputFile(out_bam_file),
+            mgd.TempSpace('sort_tmp'),
+        ),
+        kwargs={'threads': sambamba_threads}
+    )
+
+    workflow.commandline(
+        name='index',
+        args=(
+            'samtools',
+            'index',
+            mgd.InputFile(out_bam_file),
+            mgd.OutputFile(out_bam_file + '.bai'),
+        )
+    )
