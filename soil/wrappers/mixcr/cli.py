b8f9fb8389eb77bb4348418c1cbcc1ecf2631762
Andrew Roth <andrewjlroth@gmail.com>
2017-11-08 17:42:13 +0000

    mixcr runner is a single command

--- soil/wrappers/mixcr/cli.py
+++ soil/wrappers/mixcr/cli.py
@@ -10,16 +10,9 @@ import workflows
 @click.option('-o', '--out_tsv_file', required=True, type=click.Path(resolve_path=True))
 @click.option('-t', '--threads', default=1, type=int)
 @click.option('--rna', is_flag=True)
-def tcr_analysis(fastq_file_1, fastq_file_2, out_tsv_file, threads, rna):
+def mixcr(fastq_file_1, fastq_file_2, out_tsv_file, threads, rna):
     if rna:
         workflows.create_rnaseq_workflow(fastq_file_1, fastq_file_2, out_tsv_file, threads=threads)
 
     else:
         workflows.create_basic_workflow(fastq_file_1, fastq_file_2, out_tsv_file, threads=threads)
-
-
-@click.group()
-def mixcr():
-    pass
-
-mixcr.add_command(tcr_analysis)
