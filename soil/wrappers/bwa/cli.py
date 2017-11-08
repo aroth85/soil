8b945d6bed5252e7c34d75edfd0f3d5fcc748c0b
Andrew Roth <andrewjlroth@gmail.com>
2017-11-08 17:59:27 +0000

    bugfix

--- soil/wrappers/bwa/cli.py
+++ soil/wrappers/bwa/cli.py
@@ -9,7 +9,7 @@ import workflows
 @click.option('-2', '--fastq_file_2', required=True, type=click.Path(exists=True, resolve_path=True))
 @click.option('-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True))
 @click.option('-o', '--out_bam_file', required=True, type=click.Path(resolve_path=True))
-@click.option('-t', '--threads', required=True, type=click.Path(resolve_path=True))
+@click.option('-t', '--threads', default=1, type=int)
 def mem(fastq_file_1, fastq_file_2, ref_genome_fasta_file, out_bam_file, threads):
     return workflows.create_mem_workflow(
         fastq_file_1,
