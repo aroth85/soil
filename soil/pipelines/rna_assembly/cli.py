ca01409bd4ebfb2caa5685ea3b2e768b64f85546
Andrew Roth <andrewjlroth@gmail.com>
2017-11-12 17:00:11 +0000

    adding support for assembly from fastq

--- soil/pipelines/rna_assembly/cli.py
+++ soil/pipelines/rna_assembly/cli.py
@@ -11,11 +11,54 @@ import workflows
 @click.option('-o', '--out_prefix', required=True, type=click.Path(resolve_path=True))
 @click.option('-t', '--threads', default=1, type=int)
 def from_bam(bam_file, ref_genome_fasta_file, ref_gtf_file, out_prefix, threads):
-    return workflows.create_assembly_workflow(
+    out_cds_fasta_file = out_prefix + '.cds.fasta'
+
+    out_protein_fasta_file = out_prefix + '.prot.fasta'
+
+    out_gff_file = out_prefix + '.gff3'
+
+    out_gtf_file = out_prefix + '.gtf'
+
+    return workflows.create_assembly_from_bam_workflow(
         bam_file,
         ref_genome_fasta_file,
         ref_gtf_file,
-        out_prefix,
+        out_cds_fasta_file,
+        out_protein_fasta_file,
+        out_gff_file,
+        out_gtf_file,
+        threads=threads
+    )
+
+
+@soil.utils.cli.runner
+@click.option('-1', '--fastq_file_1', required=True, type=click.Path(exists=True, resolve_path=True))
+@click.option('-2', '--fastq_file_2', required=True, type=click.Path(exists=True, resolve_path=True))
+@click.option('-g', '--ref_gtf_file', required=True, type=click.Path(exists=True, resolve_path=True))
+@click.option('-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True))
+@click.option('-o', '--out_prefix', required=True, type=click.Path(resolve_path=True))
+@click.option('-t', '--threads', default=1, type=int)
+def from_fastq(fastq_file_1, fastq_file_2, ref_genome_fasta_file, ref_gtf_file, out_prefix, threads):
+    out_bam_file = out_prefix + '.bam'
+
+    out_cds_fasta_file = out_prefix + '.cds.fasta'
+
+    out_protein_fasta_file = out_prefix + '.prot.fasta'
+
+    out_gff_file = out_prefix + '.gff3'
+
+    out_gtf_file = out_prefix + '.gtf'
+
+    return workflows.create_assembly_from_fastq_workflow(
+        fastq_file_1,
+        fastq_file_1,
+        ref_genome_fasta_file,
+        ref_gtf_file,
+        out_bam_file,
+        out_cds_fasta_file,
+        out_protein_fasta_file,
+        out_gff_file,
+        out_gtf_file,
         threads=threads
     )
 
@@ -25,3 +68,4 @@ def rna_assembly():
     pass
 
 rna_assembly.add_command(from_bam)
+rna_assembly.add_command(from_fastq)
--- soil/pipelines/rna_assembly/workflows.py
+++ soil/pipelines/rna_assembly/workflows.py
@@ -1,19 +1,72 @@
+import os
 import pypeliner
 import pypeliner.managed as mgd
 
 import soil.utils.workflow
+import soil.wrappers.star.workflows
 import soil.wrappers.stringtie.tasks
 import soil.wrappers.transdecoder.tasks
 
 
-def create_assembly_workflow(bam_file, ref_genome_fasta_file, ref_gtf_file, out_prefix, threads=1):
-    cds_fasta_file = out_prefix + '.cds.fasta'
+def create_assembly_from_fastq_workflow(
+        fastq_file_1,
+        fastq_file_2,
+        ref_genome_fasta_file,
+        ref_gtf_file,
+        out_bam_file,
+        out_cds_fasta_file,
+        out_protein_fasta_file,
+        out_gff_file,
+        out_gtf_file,
+        threads=1):
 
-    protein_fasta_file = out_prefix + '.prot.fasta'
+    workflow = pypeliner.workflow.Workflow()
 
-    gff_file = out_prefix + '.gff3'
+    workflow.subworkflow(
+        name='align',
+        func=soil.wrappers.star.workflows.create_align_workflow,
+        args=(
+            mgd.InputFile(fastq_file_1),
+            mgd.InputFile(fastq_file_2),
+            os.path.dirname(ref_genome_fasta_file),
+            mgd.OutputFile(out_bam_file)
+        ),
+        kwargs={
+            'add_xs_tag': True,
+            'align_threads': threads,
+            'sort_threads': threads
+        }
+    )
 
-    stringtie_gtf_file = out_prefix + '.gtf'
+    workflow.subworkflow(
+        name='assemble_transcriptome',
+        func=create_assembly_from_bam_workflow,
+        args=(
+            mgd.InputFile(out_bam_file),
+            mgd.InputFile(ref_genome_fasta_file),
+            mgd.InputFile(ref_gtf_file),
+            mgd.OutputFile(out_cds_fasta_file),
+            mgd.OutputFile(out_protein_fasta_file),
+            mgd.OutputFile(out_gff_file),
+            mgd.OutputFile(out_gtf_file),
+        ),
+        kwargs={
+            'threads': threads,
+        }
+    )
+
+    return workflow
+
+
+def create_assembly_from_bam_workflow(
+        bam_file,
+        ref_genome_fasta_file,
+        ref_gtf_file,
+        out_cds_fasta_file,
+        out_protein_fasta_file,
+        out_gff_file,
+        out_gtf_file,
+        threads=1):
 
     sandbox = soil.utils.workflow.get_sandbox(['stringtie', 'transdecoder'])
 
@@ -26,7 +79,7 @@ def create_assembly_workflow(bam_file, ref_genome_fasta_file, ref_gtf_file, out_
         args=(
             mgd.InputFile(bam_file),
             mgd.InputFile(ref_gtf_file),
-            mgd.OutputFile(stringtie_gtf_file),
+            mgd.OutputFile(out_gtf_file),
         ),
         kwargs={
             'threads': threads
@@ -38,11 +91,11 @@ def create_assembly_workflow(bam_file, ref_genome_fasta_file, ref_gtf_file, out_
         ctx={'mem': 8, 'mem_retry_increment': 8, 'num_retry': 3},
         func=soil.wrappers.transdecoder.tasks.gtf_to_fasta,
         args=(
-            mgd.InputFile(stringtie_gtf_file),
+            mgd.InputFile(out_gtf_file),
             mgd.InputFile(ref_genome_fasta_file),
-            mgd.OutputFile(cds_fasta_file),
-            mgd.OutputFile(protein_fasta_file),
-            mgd.OutputFile(gff_file),
+            mgd.OutputFile(out_cds_fasta_file),
+            mgd.OutputFile(out_protein_fasta_file),
+            mgd.OutputFile(out_gff_file),
             mgd.TempSpace('transdecoder_tmp'),
         )
     )
