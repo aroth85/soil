4815c91bbdabff79a3ae30d5a82df905589b21d0
Andrew Roth <andrewjlroth@gmail.com>
2017-11-21 09:15:01 +0000

    adding ability to align multiple lanes with star and add read group info

--- soil/wrappers/star/cli.py
+++ soil/wrappers/star/cli.py
@@ -6,27 +6,77 @@ import workflows
 
 
 @soil.utils.cli.runner
-@click.option('-1', '--fastq_file_1', required=True, type=click.Path(exists=True, resolve_path=True))
-@click.option('-2', '--fastq_file_2', required=True, type=click.Path(exists=True, resolve_path=True))
-@click.option('-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True))
-@click.option('-o', '--out_bam_file', required=True, type=click.Path(resolve_path=True))
-@click.option('-t', '--threads', default=1, type=int)
 @click.option(
-    '-x',
-    '--add_xs_tag',
-    is_flag=True,
-    help='''Set this to support downwstream cufflinks and stringtie analysis.'''
+    '-f', '---fastq_files', required=True, multiples=True, nargs=2, type=click.Path(exists=True, resolve_path=True),
+    help='''A pair of file paths for 1 and 2 file from paired end sequencing. Can be specified multiple times if
+    multiple lanes where run.'''
 )
-def align(fastq_file_1, fastq_file_2, ref_genome_fasta_file, out_bam_file, add_xs_tag, threads):
+@click.option(
+    '-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True),
+    help='''Path to reference genome in FASTA format to align against. STAR index files should be in same directory.'''
+)
+@click.option(
+    '-o', '--out_bam_file', required=True, type=click.Path(resolve_path=True),
+    help='''Path where output will be written in coordinate sorted duplicate marked BAM format.'''
+)
+@click.option(
+    '-l', '--library_id', default=None, type=str,
+    help='''Name of library sequenced to create FASTQ files.'''
+)
+@click.option(
+    '-r', '--read_group_ids', default=None, multiple=True, type=str,
+    help='''Read group ID to be used for the lanes. Should be set to match -f, that is the same order and number of
+    times. If not set it will be guessed from the file name.'''
+)
+@click.option(
+    '-s', '--sample_id', default=None, type=str,
+    help='''Name of sample used to prepare library.'''
+)
+@click.option(
+    '-t', '--threads', default=1, type=int,
+    help='''Number of threads used in parallel steps of workflow.'''
+)
+@click.option(
+    '-x', '--add_xs_tag', is_flag=True,
+    help='''Add XS tag to BAM alignment. Set this to support downwstream cufflinks and stringtie analysis.'''
+)
+def align(fastq_files, ref_genome_fasta_file, out_bam_file, add_xs_tag, library_id, read_group_ids, sample_id, threads):
+    fastq_files_1 = {}
+
+    fastq_files_2 = {}
+
+    read_group_info = {}
+
+    if library_id is None:
+        library_id = 'UKNOWN'
+
+    if sample_id is None:
+        sample_id = 'UKNOWN'
+
+    for f1, f2 in fastq_files:
+        if read_group_ids is not None:
+            key = read_group_ids.pop(0)
+
+        else:
+            key = os.path.basename(f1).split('.')[0]
+
+        fastq_files_1[key] = f1
+
+        fastq_files_2[key] = f2
+
+        read_group_info[key] = {'ID': key, 'LB': library_id, 'SM': sample_id}
+
     ref_genome_fasta_dir = os.path.dirname(ref_genome_fasta_file)
 
-    return workflows.create_align_workflow(
-        fastq_file_1,
-        fastq_file_2,
+    return workflows.create_multiple_lane_align_workflow(
+        fastq_files_1,
+        fastq_files_2,
         ref_genome_fasta_dir,
         out_bam_file,
         add_xs_tag=add_xs_tag,
         align_threads=threads,
+        merge_threads=threads,
+        read_group_info=read_group_info,
         sort_threads=threads,
     )
 
