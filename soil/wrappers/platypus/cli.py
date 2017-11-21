b9c5172454d0ed1add8221baa1543c867d96f10f
Andrew Roth <andrewjlroth@gmail.com>
2017-11-21 10:50:56 +0000

    cleaning up platypus cli interface

--- soil/wrappers/platypus/cli.py
+++ soil/wrappers/platypus/cli.py
@@ -5,13 +5,28 @@ import workflows
 
 
 @soil.utils.cli.runner
-@click.option('-i', '--bam_file', required=True, type=click.Path(exists=True, resolve_path=True))
-@click.option('-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True))
-@click.option('-o', '--out_vcf_file', required=True, type=click.Path(resolve_path=True))
-@click.option('-c', '--chromosomes', multiple=True, type=str)
-@click.option('-s', '--split_size', default=int(1e7), type=int)
-@click.option('--rna', is_flag=True)
-def single_sample(bam_file, ref_genome_fasta_file, out_vcf_file, chromosomes, split_size, rna):
+@click.option(
+    '-b', '--bam_file', required=True, type=click.Path(exists=True, resolve_path=True),
+    help='''Path of BAM file to call variants from.'''
+)
+@click.option(
+    '-r', '--ref_genome_fasta_file', required=True, type=click.Path(exists=True, resolve_path=True),
+    help='''Path of reference genome FASTA file BAM file was aligned to.'''
+)
+@click.option(
+    '-o', '--out_vcf_file', required=True, type=click.Path(resolve_path=True),
+    help='''Path where output file will be written in bgzip compressed VCF format.'''
+)
+@click.option(
+    '-c', '--chromosomes', multiple=True, type=str,
+    help='''Chromosome to analyze. Can be specified multiple times i.e. -c chr1 -c chrX to analyze chromosomes 1 and X.
+    '''
+)
+@click.option(
+    '--rna', is_flag=True,
+    help='''Set this flag if the BAM file contains RNA-Seq data. Will pre-process the files with opossum.'''
+)
+def single_sample(bam_file, ref_genome_fasta_file, out_vcf_file, chromosomes, rna):
     if len(chromosomes) == 0:
         chromosomes = None
 
@@ -21,7 +36,6 @@ def single_sample(bam_file, ref_genome_fasta_file, out_vcf_file, chromosomes, sp
             ref_genome_fasta_file,
             out_vcf_file,
             chromosomes=chromosomes,
-            split_size=split_size
         )
 
     else:
@@ -30,7 +44,6 @@ def single_sample(bam_file, ref_genome_fasta_file, out_vcf_file, chromosomes, sp
             ref_genome_fasta_file,
             out_vcf_file,
             chromosomes=chromosomes,
-            split_size=split_size
         )
 
 
