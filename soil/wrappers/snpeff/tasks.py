42592fead91752b3fa9216d4ad363ddd8914bfe1
Andrew Roth <andrewjlroth@gmail.com>
2017-12-08 16:29:48 +0000

    snpeff option to pass data dir

--- soil/wrappers/snpeff/tasks.py
+++ soil/wrappers/snpeff/tasks.py
@@ -2,7 +2,7 @@ import os
 import pypeliner.commandline as cli
 
 
-def snpeff(db, in_vcf_file, out_file):
+def snpeff(db, in_vcf_file, out_file, data_dir=None):
     os.environ['MALLOC_ARENA_MAX'] = '2'
 
     cmd = [
@@ -12,10 +12,17 @@ def snpeff(db, in_vcf_file, out_file):
         '-Xms2g',
         '-Xmx5g',
         '-hgvs1LetterAa',
+    ]
+
+    if data_dir is not None:
+        cmd.extend(['-dataDir', data_dir])
+        cmd.append('-nodownload')
+
+    cmd.extend([
         db,
         in_vcf_file,
         '>',
         out_file
-    ]
+    ])
 
     cli.execute(*cmd)
