fd303f8c78183f2e5b33a6094685d1a0cb8a3792
Andrew Roth <andrewjlroth@gmail.com>
2017-12-08 16:23:12 +0000

    bugfix: copy in unzip

--- soil/ref_data/tasks.py
+++ soil/ref_data/tasks.py
@@ -95,7 +95,14 @@ def unzip_file(in_file, out_sentinel, tmp_dir):
 
     out_dir = os.path.dirname(out_sentinel)
 
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+
+    shutil.copytree(tmp_dir, out_dir)
+
     for x in os.listdir(tmp_dir):
         shutil.move(x, out_dir)
 
     open(out_sentinel, 'w').close()
+
+    shutil.rmtree(tmp_dir)
