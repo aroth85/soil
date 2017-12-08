4bd4f86add06e4646c688e9d75f1afdb5cc632e4
Andrew Roth <andrewjlroth@gmail.com>
2017-12-08 15:48:02 +0000

    bugfix: workflow issues

--- soil/ref_data/workflows.py
+++ soil/ref_data/workflows.py
@@ -215,8 +215,6 @@ def create_download_decompress_workflow(url, local_path):
     workflow = pypeliner.workflow.Workflow()
 
     if os.path.exists(local_path):
-        workflow.commandline(name='sleep', args=('sleep', 1))
-
         return workflow
 
     workflow.setobj(mgd.TempOutputObj('url'), value=url)
