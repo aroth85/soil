b57018a5b197a548f52897bdddd4c5bb9705ce25
Andrew Roth <andrewjlroth@gmail.com>
2018-01-24 16:00:15 +0000

    refactor: moving titan allele counts to subworkflow

--- soil/wrappers/titan/workflows.py
+++ soil/wrappers/titan/workflows.py
@@ -17,7 +17,7 @@ def create_titan_workflow(
         exome_bed_file=None,
         sample='Tumour'):
 
-    sandbox = soil.utils.workflow.get_sandbox(['hmmcopy', 'hmmcopy_utils', 'snpsift', 'titan'])
+    sandbox = soil.utils.workflow.get_sandbox(['hmmcopy', 'hmmcopy_utils', 'titan'])
 
     sandbox.packages.extend(['pandas', 'rpy2'])
 
@@ -31,89 +31,18 @@ def create_titan_workflow(
     )
 
     workflow.subworkflow(
-        name='call_snps',
-        func=soil.wrappers.platypus.workflows.create_single_sample_workflow,
+        name='get_allele_counts',
+        func=create_allele_counts_workflow,
         args=(
             mgd.InputFile(normal_bam_file),
-            mgd.InputFile(ref_genome_fasta_file),
-            mgd.TempOutputFile('normal.vcf.gz'),
-        ),
-        kwargs={
-            'chromosomes': chromosomes,
-            'split_size': int(1e7),
-        }
-    )
-
-    workflow.commandline(
-        name='annotate_dbsnp_status',
-        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
-        args=(
-            'SnpSift',
-            'annotate',
-            mgd.InputFile(dbsnp_vcf_file),
-            mgd.TempInputFile('normal.vcf.gz'),
-            '>',
-            mgd.TempOutputFile('normal.dbsnp.vcf'),
-        ),
-    )
-
-    workflow.commandline(
-        name='annotate_variant_type',
-        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
-        args=(
-            'SnpSift',
-            'varType',
-            mgd.TempInputFile('normal.dbsnp.vcf'),
-            '>',
-            mgd.TempOutputFile('normal.dbsnp.vartype.vcf'),
-        ),
-    )
-
-    workflow.commandline(
-        name='filter_het_snps',
-        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
-        args=(
-            'SnpSift',
-            'filter',
-            "isHet(GEN[0]) & ((exists ID) & ( ID =~ 'rs' )) & (exists SNP)",
-            mgd.TempInputFile('normal.dbsnp.vartype.vcf'),
-            '>',
-            mgd.TempOutputFile('het.snps.vcf'),
-        ),
-    )
-
-    workflow.transform(
-        name='split_vcf',
-        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
-        func=tasks.split_vcf,
-        args=(
-            mgd.TempInputFile('het.snps.vcf'),
-            mgd.TempOutputFile('split.vcf', 'split'),
-            mgd.TempSpace('split_tmp')
-        ),
-        kwargs={
-            'split_size': int(1e4),
-        }
-    )
-
-    workflow.transform(
-        name='get_allele_counts',
-        axes=('split',),
-        func=tasks.get_snv_allele_counts_for_vcf_targets,
-        args=(
             mgd.InputFile(tumour_bam_file),
-            mgd.TempInputFile('split.vcf', 'split'),
-            mgd.TempOutputFile('split.tsv', 'split'),
-        )
-    )
-
-    workflow.transform(
-        name='merge_counts',
-        func=tasks.merge_counts,
-        args=(
-            mgd.TempInputFile('split.tsv', 'split'),
-            mgd.TempOutputFile('allele_counts.tsv'),
-        )
+            mgd.InputFile(dbsnp_vcf_file),
+            mgd.InputFile(ref_genome_fasta_file),
+            mgd.TempOutputFile('allele_counts.tsv')
+        ),
+        kwargs={
+            'chromosomes': 'autosomes'
+        }
     )
 
     workflow.commandline(
@@ -123,7 +52,7 @@ def create_titan_workflow(
             '-c', ','.join(chromosomes),
             mgd.InputFile(normal_bam_file),
             '>',
-            mgd.TempOutputFile('normal.wig'),
+            mgd.TempOutputFile('normal.wig')
         )
     )
 
@@ -134,7 +63,7 @@ def create_titan_workflow(
             '-c', ','.join(chromosomes),
             mgd.InputFile(tumour_bam_file),
             '>',
-            mgd.TempOutputFile('tumour.wig'),
+            mgd.TempOutputFile('tumour.wig')
         )
     )
 
@@ -145,7 +74,7 @@ def create_titan_workflow(
             '-c', ','.join(chromosomes),
             mgd.InputFile(ref_genome_fasta_file),
             '>',
-            mgd.TempOutputFile('gc.wig'),
+            mgd.TempOutputFile('gc.wig')
         )
     )
 
@@ -156,7 +85,7 @@ def create_titan_workflow(
             '-c', ','.join(chromosomes),
             mgd.InputFile(mappability_file),
             '>',
-            mgd.TempOutputFile('mappability.wig'),
+            mgd.TempOutputFile('mappability.wig')
         )
     )
 
@@ -168,10 +97,10 @@ def create_titan_workflow(
             mgd.TempInputFile('tumour.wig'),
             mgd.TempInputFile('gc.wig'),
             mgd.TempInputFile('mappability.wig'),
-            mgd.TempOutputFile('coverage.wig'),
+            mgd.TempOutputFile('coverage.wig')
         ),
         kwargs={
-            'target_file': exome_bed_file,
+            'target_file': exome_bed_file
         }
     )
 
@@ -185,11 +114,11 @@ def create_titan_workflow(
             mgd.TempInputFile('allele_counts.tsv'),
             mgd.TempInputObj('init_params', 'param_idx'),
             mgd.TempOutputFile('run.tar.gz', 'param_idx'),
-            mgd.TempSpace('titan_tmp', 'param_idx'),
+            mgd.TempSpace('titan_tmp', 'param_idx')
         ),
         kwargs={
             'is_exome': (exome_bed_file is not None),
-            'sample': sample,
+            'sample': sample
         }
     )
 
@@ -199,8 +128,8 @@ def create_titan_workflow(
         args=(
             mgd.TempInputFile('run.tar.gz', 'param_idx'),
             mgd.TempInputObj('init_params', 'param_idx'),
-            mgd.TempOutputFile('stats.tsv'),
-        ),
+            mgd.TempOutputFile('stats.tsv')
+        )
     )
 
     workflow.transform(
@@ -212,7 +141,110 @@ def create_titan_workflow(
             mgd.TempInputFile('run.tar.gz', 'param_idx'),
             mgd.TempInputFile('stats.tsv'),
             mgd.OutputFile(out_file),
-            mgd.TempSpace('build_results'),
+            mgd.TempSpace('build_results')
+        )
+    )
+
+    return workflow
+
+
+def create_allele_counts_workflow(
+        normal_bam_file,
+        tumour_bam_file,
+        dbsnp_vcf_file,
+        ref_genome_fasta_file,
+        allele_counts_file,
+        chromosomes='autosomes'):
+
+    chromosomes = soil.utils.genome.load_bam_chromosome_lengths(normal_bam_file, chromosomes)
+
+    sandbox = soil.utils.workflow.get_sandbox(['snpsift'])
+
+    workflow = pypeliner.workflow.Workflow(default_sandbox=sandbox)
+
+    workflow.subworkflow(
+        name='call_snps',
+        func=soil.wrappers.platypus.workflows.create_single_sample_workflow,
+        args=(
+            mgd.InputFile(normal_bam_file),
+            mgd.InputFile(ref_genome_fasta_file),
+            mgd.TempOutputFile('normal.vcf.gz'),
+        ),
+        kwargs={
+            'chromosomes': chromosomes,
+            'split_size': int(1e7)
+        }
+    )
+
+    workflow.commandline(
+        name='annotate_dbsnp_status',
+        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
+        args=(
+            'SnpSift',
+            'annotate',
+            mgd.InputFile(dbsnp_vcf_file),
+            mgd.TempInputFile('normal.vcf.gz'),
+            '>',
+            mgd.TempOutputFile('normal.dbsnp.vcf')
+        )
+    )
+
+    workflow.commandline(
+        name='annotate_variant_type',
+        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
+        args=(
+            'SnpSift',
+            'varType',
+            mgd.TempInputFile('normal.dbsnp.vcf'),
+            '>',
+            mgd.TempOutputFile('normal.dbsnp.vartype.vcf')
+        )
+    )
+
+    workflow.commandline(
+        name='filter_het_snps',
+        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
+        args=(
+            'SnpSift',
+            'filter',
+            "isHet(GEN[0]) & ((exists ID) & ( ID =~ 'rs' )) & (exists SNP)",
+            mgd.TempInputFile('normal.dbsnp.vartype.vcf'),
+            '>',
+            mgd.TempOutputFile('het.snps.vcf')
+        )
+    )
+
+    workflow.transform(
+        name='split_vcf',
+        ctx={'mem': 6, 'mem_retry_increment': 4, 'num_retry': 3},
+        func=tasks.split_vcf,
+        args=(
+            mgd.TempInputFile('het.snps.vcf'),
+            mgd.TempOutputFile('split.vcf', 'split'),
+            mgd.TempSpace('split_tmp')
+        ),
+        kwargs={
+            'split_size': int(1e4)
+        }
+    )
+
+    workflow.transform(
+        name='get_allele_counts',
+        axes=('split',),
+        func=tasks.get_snv_allele_counts_for_vcf_targets,
+        args=(
+            mgd.InputFile(tumour_bam_file),
+            mgd.TempInputFile('split.vcf', 'split'),
+            mgd.TempOutputFile('split.tsv', 'split')
+        )
+    )
+
+    workflow.transform(
+        name='merge_counts',
+        func=tasks.merge_counts,
+        args=(
+            mgd.TempInputFile('split.tsv', 'split'),
+            mgd.OutputFile(allele_counts_file)
         )
     )
 
